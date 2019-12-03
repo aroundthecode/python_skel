39ccf49fe058048f71b7de908a394c9e8369208a
Michele Sacchetti <michele.sacchetti@aroundthecode.org>
2019-12-03 23:46:28 +0100

    working rename.sh

--- rename.sh
+++ rename.sh
@@ -5,11 +5,11 @@ echo
 read -p 'New project name: ' NEWNAME
 
 echo "Patching test import"
-sed -i '' 's/myproject/${NEWNAME}/' test/test_main.py
+sed -i '' 's/myproject/'${NEWNAME}'/' tests/test_main.py
 echo "Patching setup.cfg"
-sed -i '' 's/myproject/${NEWNAME}/' setup.cfg
+sed -i '' 's/myproject/'${NEWNAME}'/' setup.cfg
 echo "Patching Makefile"
-sed -i '' 's/myproject/${NEWNAME}/' Makefile
+sed -i '' 's/myproject/'${NEWNAME}'/' Makefile
 echo "Renaming folder"
 mv myproject ${NEWNAME}
 echo "DONE, remember to edit setup.cfg and README.md for additional details on your project"
