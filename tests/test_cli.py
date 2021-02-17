52b9f027fd6b645b3502a947c4bf45cee8d2d5e9
Michele Sacchetti <michele.sacchetti@aroundthecode.org>
2021-02-17 19:25:44 +0100

    last cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 import pytest
 import myproject.cli as cli
+from myproject.utils.webcfg import Config
 
 
 @pytest.mark.parametrize("cli_method, help_txt", [
@@ -14,3 +15,21 @@ def test_cli_help(cli_method, help_txt):
     result = runner.invoke(cli_method, ["-h"])
     assert result.exit_code == 0
     assert help_txt in result.output
+
+
+@pytest.mark.unit
+def test_cli_version():
+    runner = CliRunner()
+
+    result = runner.invoke(cli.version, [])
+    assert result.exit_code == 0
+    assert result.output.strip() == Config.get_version()
+
+
+@pytest.mark.unit
+def test_cli_sample():
+    runner = CliRunner()
+
+    result = runner.invoke(cli.sample, [])
+    assert result.exit_code == 0
+    assert "Hello" in result.output.strip()
