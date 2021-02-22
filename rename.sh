33adc26f8f31245bf20bfe88a87b3afd73ec8d27
Michele Sacchetti <michele.sacchetti@aroundthecode.org>
2021-02-22 10:11:37 +0100

    rename.sh crawl full myproject folder

--- rename.sh
+++ rename.sh
@@ -4,12 +4,20 @@ echo "This script will rename all significat project section replacing 'myprojec
 echo
 read -p 'New project name: ' NEWNAME
 
-echo "Patching test import"
-sed -i '' 's/myproject/'${NEWNAME}'/' tests/test_main.py
+
+for i in $(grep -Rl ${NEWNAME} myproject | grep -v "__pycache__")
+do
+    echo "Patching [${i}]"
+    sed -i '' 's/myproject/'${NEWNAME}'/' ${i}
+done;
+
 echo "Patching setup.cfg"
 sed -i '' 's/myproject/'${NEWNAME}'/' setup.cfg
+
 echo "Patching Makefile"
 sed -i '' 's/myproject/'${NEWNAME}'/' Makefile
+
 echo "Renaming folder"
 mv myproject ${NEWNAME}
+
 echo "DONE, remember to edit setup.cfg and README.md for additional details on your project"
